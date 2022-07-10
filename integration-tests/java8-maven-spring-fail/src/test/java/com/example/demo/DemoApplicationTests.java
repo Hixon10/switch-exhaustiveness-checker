4c8e7b64d34293e9cddcda353b970acc73abea2f
Denis <Hixon10@yandex.ru>
2022-07-10 13:16:18 +0100

    Add maven integration tests

---
+++ integration-tests/java8-maven-spring-fail/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void contextLoads() {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello",
+				String.class)).isEqualTo("return from switch");
+	}
+
+}
